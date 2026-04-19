969d7680f631f0dd9ac9baba36459f0baad0136d
mujeebullahkhugianiwal <mujeebullahabdulzai@gmail.com>
2026-04-20 01:13:59 +0200

    new messag

--- dockerfile
+++ dockerfile
@@ -1,7 +1,7 @@
 FROM php:8.2-apache
 
 # Install mysqli
-RUN docker-php-ext-install mysqli
+RUN docker-php-ext-install mysqli pdo pdo_mysql
 
 # Enable Apache mod_rewrite (optional but useful)
 RUN a2enmod rewrite
--- index.php
+++ index.php
@@ -1,15 +1,42 @@
 <?php
+class Database
+{
 
-$host = 'db'; // IMPORTANT: service name from docker-compose
-$user = 'root';
-$password = 'root';
-$database = 'test_db';
+    private $conniction;
+    private $stmt;
 
-$conn = new mysqli($host, $user, $password, $database);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    public function __construct()
+    {
+        $host = "db";
+        $user = "root";
+        $password = "root";
+        $db = "test_db";
+        try {
+            $this->conniction = new PDO("mysql:host={$host};dbname={$db};charset=utf8mb4", $user, $password, [
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+            echo "database connected successfully";
+        } catch (PDOException $e) {
+            echo "datbase connection faild " . $e->getMessage();
+        }
+    }
+    public function query($sql, $arr = [])
+    {
+        $this->stmt = $this->conniction->prepare($sql);
+        $this->stmt->execute($arr);
+        return $this;
+    }
+    public function fetch()
+    {
+        return $this->stmt->fetch();
+    }
+    public function fetchall()
+    {
+        return $this->stmt->fetchAll();
+    }
 }
 
-
-echo "Connected successfully!";
+$obj = new Database();
+var_dump($obj->query("select * from userTable")->fetch());
